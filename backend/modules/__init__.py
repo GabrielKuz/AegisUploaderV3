292e32bb4d77324d41870066a82d35577a4978d2
Ben Bell <110692534+slowpoke111@users.noreply.github.com>
2026-07-07 09:09:44 -0400

    Update startup script

--- backend/modules/__init__.py
+++ backend/modules/__init__.py
@@ -4,6 +4,7 @@ import os
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
 
+
 engine = create_engine(os.environ['DATABASE_URL'],)
 Session = sessionmaker(bind=engine) # Create a main session factory to prevent multiple engine instances
 
