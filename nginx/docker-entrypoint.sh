ad1c5590dd78b494b282738886786079c7bd793a
GabrielKuzowsky <148278698+GabrielKuz@users.noreply.github.com>
2026-07-15 11:00:27 -0400

    entrypoint tweaks

--- nginx/docker-entrypoint.sh
+++ nginx/docker-entrypoint.sh
@@ -1,12 +1,4 @@
 #!/bin/sh
-# set -e
-
-# envsubst '$BACKEND_URL' \
-#     < /etc/nginx/nginx.conf.template \
-#     > /etc/nginx/nginx.conf
-
-# exec "$@"
-
 set -e
 
 echo "BACKEND_URL IS: [$BACKEND_URL]"
