272ddf5d34e302574be748b760b15ed085ff6f5b
GabrielKuzowsky <148278698+GabrielKuz@users.noreply.github.com>
2026-07-16 14:02:32 -0400

    docker entrypoint once again

--- nginx/docker-entrypoint.sh
+++ nginx/docker-entrypoint.sh
@@ -8,12 +8,11 @@ if [ "$BUILD_TYPE" = "dev" ]; then
         < /etc/nginx/nginx.conf.template \
         > /etc/nginx/nginx.conf
     echo "Using dev backend URL: $DEV_BACKEND_URL"
-else if [ "$BUILD_TYPE" = "production" ]; then
+elif [ "$BUILD_TYPE" = "production" ]; then
     envsubst '$PROD_BACKEND_URL' \
         < /etc/nginx/nginx.conf.template \
         > /etc/nginx/nginx.conf
     echo "Using prod backend URL: $PROD_BACKEND_URL"
+fi
 
 exec "$@"
-
-fi
